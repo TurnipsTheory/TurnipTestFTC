cc9b464a05e1cfd92e882f0dec6ebb6189119f8c
TurnipsTheory <zarabara923@gmail.com>
2025-09-20 13:42:28 -0700

    Update DriveTrainTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTest.java
@@ -65,6 +65,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
+
+// yo whats up my sigma
 @TeleOp(name="DriveTrainTest", group="Linear OpMode")
 
 public class DriveTrainTest extends LinearOpMode {
