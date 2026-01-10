926588f17f89baff1fca8171a17b92873e26ed93
Kisutz <danm2nfamily@gmail.com>
2026-01-10 11:17:01 -0800

    claude writes april tag test and i pray:>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/EasyOpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/EasyOpenCVTest.java
@@ -8,39 +8,50 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvWebcam;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.opencv.core.Mat;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 @TeleOp(name="Webcam Vision Test", group="Vision")
-public class VisionOpMode extends LinearOpMode {
+public class EasyOpenCVTest extends LinearOpMode {
 //example code taken from Chat
-//    OpenCvWebcam webcam;
-//
-//    @Override
-//    public void runOpMode() {
-//        int cameraMonitorViewId = hardwareMap.appContext
-//                .getResources().getIdentifier("cameraMonitorViewId","id",
-//                        hardwareMap.appContext.getPackageName());
-//
-//        webcam = OpenCvCameraFactory.getInstance()
-//                .createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"),
-//                        cameraMonitorViewId);
-//
-//        webcam.setPipeline(new OpenCvPipeline() {
-//            @Override
-//            public Mat processFrame(Mat input) {
-//                return input; // no processing yet
-//            }
-//        });
-//
-//        webcam.openCameraDeviceAsync(() -> {
-//            webcam.startStreaming(640, 480);
-//        });
-//
-//        waitForStart();
-//
-//        while (opModeIsActive()) {
-//            telemetry.addLine("Camera streaming...");
-//            telemetry.update();
-//            sleep(50);
-//        }
-//    }
-//}
+    OpenCvWebcam webcam;
+
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier("cameraMonitorViewId","id",
+                        hardwareMap.appContext.getPackageName());
+
+        webcam = OpenCvCameraFactory.getInstance()
+                .createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"),
+                        cameraMonitorViewId);
+
+        webcam.setPipeline(new OpenCvPipeline() {
+            @Override
+            public Mat processFrame(Mat input) {
+                return input; // no processing yet
+            }
+        });
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(640, 480);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Error", "Camera failed to open with error code: " + errorCode);
+                telemetry.update();
+            }
+        });
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addLine("Camera streaming...");
+            telemetry.update();
+            sleep(50);
+        }
+    }
+}
+
