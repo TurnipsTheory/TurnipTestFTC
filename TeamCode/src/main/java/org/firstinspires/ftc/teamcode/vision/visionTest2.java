fa15362ed040e42a3ca1bfa6d2bc358b6ce04fbb
Kisutz <danm2nfamily@gmail.com>
2026-01-15 20:42:19 -0800

    PID TUNED!!!!!!!!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/visionTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/visionTest2.java
@@ -4,11 +4,12 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
+import org.firstinspires.ftc.teamcode.teleOp.RTPAxon;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -27,65 +28,66 @@ import java.util.Locale;
 @TeleOp(name="visionTest2", group="Vision")
 
 public class visionTest2 extends LinearOpMode {
+    private RTPAxon axon;
+
     private VisionPortal visionPortal;
     private AprilTagProcessor aprilTag;
     double bearingVal = 0;
     double xVal = 0;
     int direction = 0;
-    private CRServo turretServo = null;
-    private double power = 0.0;
-
-    // Smoothing variables
-    private double smoothedBearing = 0;
-    private long lastDetectionTime = 0;
-    private static final double SMOOTHING_FACTOR = 0.9;  // 0.0 = no smoothing, 1.0 = max smoothing
-    private int times = 1;  // Track last power for derivative control
-
-
+    CRServo turretServo = hardwareMap.get(CRServo.class, "turret_servo");
+    AnalogInput turretEncoder = hardwareMap.get(AnalogInput.class, "turret_servo_encoder");
+    axon = new RTPAxon(turretServo, turretEncoder);
 
 
     public void runOpMode() {
         turretServo = hardwareMap.get(CRServo.class, "turret_servo");
         initAprilTag();
 
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
-
         while (opModeIsActive()) {
             // Get list of detected AprilTags
             List<AprilTagDetection> detections = aprilTag.getDetections();
-
+//
             telemetry.addData("# AprilTags Detected", detections.size());
-
-            // Display info for each detected tag
+//
+//            // Display info for each detected tag
             for (AprilTagDetection detection : detections) {
                 if (detection.metadata != null && detection.ftcPose != null) {
-                    telemetry.addLine(String.format(Locale.US, "\n==== Tag ID %d (%s) ====",
-                            detection.id, detection.metadata.name));
-                    telemetry.addLine(String.format(Locale.US, "XYZ %6.1f %6.1f %6.1f  (inch)",
-                            detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                    telemetry.addLine(String.format(Locale.US, "PRY %6.1f %6.1f %6.1f  (deg)",
-                            detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+//                    telemetry.addLine(String.format(Locale.US, "\n==== Tag ID %d (%s) ====",
+//                            detection.id, detection.metadata.name));
+//                    telemetry.addLine(String.format(Locale.US, "XYZ %6.1f %6.1f %6.1f  (inch)",
+//                            detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+//                    telemetry.addLine(String.format(Locale.US, "PRY %6.1f %6.1f %6.1f  (deg)",
+//                            detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                     telemetry.addLine(String.format(Locale.US, "RBE %6.1f %6.1f %6.1f  (inch, deg, deg)",
                             detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
                     bearingVal = detection.ftcPose.bearing;
                     xVal = detection.ftcPose.x;
-
+//
                 } else {
                     telemetry.addLine(String.format(Locale.US, "\n==== Unknown Tag ID %d ====", detection.id));
                     telemetry.addLine(String.format(Locale.US, "Center %6.0f %6.0f   (pixels)",
                             detection.center.x, detection.center.y));
                 }
             }
+//
+//            telemetry.addLine("\nKey:\nXYZ = Translation");
+//            telemetry.addLine("PRY = Pitch, Roll, Yaw (Rotation)");
+//            telemetry.addLine("RBE = Range, Bearing, Elevation");
+            if (detections.isEmpty()) {
+                turretServo.setDirection(turretServo.getDirection());
+                turretServo.setPower(-0.06);
+                telemetry.addData("searching #", bearingVal);
 
-            telemetry.addLine("\nKey:\nXYZ = Translation");
-            telemetry.addLine("PRY = Pitch, Roll, Yaw (Rotation)");
-            telemetry.addLine("RBE = Range, Bearing, Elevation");
-
-            turretCenter();
-
+            }
+            else {
+                turretCenter();
+            }
             telemetry.update();
         }
 
@@ -95,20 +97,21 @@ public class visionTest2 extends LinearOpMode {
     }
 
     public void turretCenter() {
-        telemetry.addLine(String.format(Locale.US,"running centering of turret %f", power));
-        turretServo.setPower(1.0);
-        if (times == 1) {
-            power = 1.0;
-            turretServo.setDirection(CRServo.Direction.FORWARD);
-            times += 1;
-        } else if (times == 100) {
-            power = -1.0;
-            turretServo.setDirection(CRServo.Direction.REVERSE);
-            times -= 1;
-        } else if (times > 1 ) {
-            times = times + (int) Math.round(power);
+//        power = Math.min(0.6, Math.max(Math.abs(bearingVal/100), 0.06));
+        if (bearingVal < -0.5) {
+            turretServo.setPower(-1.0);
+        } else if (bearingVal > 0.5) {
+            turretServo.setPower(1.0);
+        } else {
+            turretServo.setPower(0.0);
         }
+        telemetry.addData("running centering of turret #", bearingVal);
+        telemetry.addLine(String.format(Locale.US, "power: %f", power));
+
+
     }
+
+
 
 
     /**
