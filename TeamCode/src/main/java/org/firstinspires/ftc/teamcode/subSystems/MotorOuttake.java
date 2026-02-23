ded415239cfa78d3671581373b7749600c10423f
TurnipsTheory <zarabara923@gmail.com>
2026-02-22 23:04:12 -0800

    Revert "Install RoadRunner v0.5 - Add maven repository and enable multiDex"
    This reverts commit 8ddf8933291087d45b8c228e8f5f24709114d4ef, reversing
    changes made to 2ac6b171064a121c45abf4e19169b2860e7c93d3.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/MotorOuttake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/MotorOuttake.java
@@ -64,16 +64,16 @@ public class MotorOuttake extends Subsystem {
 //            turretServo.setPower(0.0);
 //        }
 //    }
-    double power = 0.53;
+    double power = 0.65;
     public void runOuttake() {
         if(gamepad1.rightBumperWasPressed()){
-            power += 0.01;
+            power += 0.1;
             if(power > 1.0){
                 power = 1.0;
             }
         }
         else if(gamepad1.leftBumperWasPressed()){
-            power -= 0.01;
+            power -= 0.1;
             if(power < 0.0){
                 power = 0.0;
             }
