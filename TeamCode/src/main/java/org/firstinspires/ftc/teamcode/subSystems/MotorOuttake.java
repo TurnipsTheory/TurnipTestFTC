c3633e1f79246704da6009419e52bd27216e06c5
Kisutz <danm2nfamily@gmail.com>
2026-02-28 12:33:48 -0800

    pid tuned outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/MotorOuttake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/MotorOuttake.java
@@ -1,85 +1,58 @@
 package org.firstinspires.ftc.teamcode.subSystems;
 
-import com.acmerobotics.roadrunner.ftc.PositionVelocityPair;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.acmerobotics.roadrunner.ftc.Encoder;
-import com.acmerobotics.roadrunner.ftc.RawEncoder;
-import com.acmerobotics.roadrunner.ftc.OverflowEncoder;
-
-
-//import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-//import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-//
-//
-//import java.util.List;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//
-//import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-//import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-//import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-//import org.firstinspires.ftc.vision.VisionPortal;
-//
-//import java.util.Locale;
-
-import java.util.Locale;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
 import robotcore.Subsystem;
 
 
 public class MotorOuttake extends Subsystem {
 
     boolean isOn;
-    public DcMotor OuttakeMotorRight = null;
-    public DcMotor OuttakeMotorLeft = null;
+    public DcMotorEx OuttakeMotorLeft, OuttakeMotorRight;
 
 
-//    private AprilTagProcessor aprilTag;
-//    List<AprilTagDetection> detection = aprilTag.getDetections();
-    //double bearingVal = AprilTagTest.camBearing;
+    double lastError = 0;
+    double integralSum = 0;
+    double rpm = 3000;
+
+    ElapsedTime timer = new ElapsedTime();
+
 
     @Override
     public void init(OpMode opMode) {
         instantiateSubsystem(opMode);
-        OuttakeMotorRight = hardwareMap.get(DcMotorEx.class, "outtake_motor_right");
-        OuttakeMotorRight.setDirection(DcMotor.Direction.REVERSE);
+
         OuttakeMotorLeft = hardwareMap.get(DcMotorEx.class, "outtake_motor_left");
-        OuttakeMotorLeft.setDirection(DcMotor.Direction.FORWARD);
-        //OuttakeMotorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        OuttakeMotorRight = hardwareMap.get(DcMotorEx.class, "outtake_motor_right");
+
+        OuttakeMotorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        OuttakeMotorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        OuttakeMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        OuttakeMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
     }
 
-    //    public void turretCenter(){
-//        if(bearingVal < 0){
-//            turretServo.setPower(1.0);
-//        }
-//        else if(bearingVal > 0){
-//            turretServo.setPower(-1.0);
-//        }
-//        else{
-//            turretServo.setPower(0.0);
-//        }
-//    }
-    double power = 0.53;
     public void runOuttake() {
         if(gamepad1.rightBumperWasPressed()){
-            power += 0.01;
-            if(power > 1.0){
-                power = 1.0;
+            rpm += 100;
+            if(rpm > 1.0){
+                rpm = 1.0;
             }
         }
         else if(gamepad1.leftBumperWasPressed()){
-            power -= 0.01;
-            if(power < 0.0){
-                power = 0.0;
+            rpm -= 100;
+            if(rpm < 0.0){
+                rpm = 0.0;
             }
         }
-        telemetry.addData("Flywheel Power: ", power);
 
+
+
+        double targetTicksPerSec = (rpm / 60.0) * 28.0;
+        double actualTicksPerSec = -1*OuttakeMotorLeft.getVelocity();
+        double actualRPM = (actualTicksPerSec / 28.0) * 60.0;
         if (gamepad2.xWasPressed()) {
             if (isOn) {
                 isOn = false;
@@ -88,8 +61,25 @@ public class MotorOuttake extends Subsystem {
                 telemetry.addLine("off");
             } else {
                 isOn = true;
-                OuttakeMotorRight.setPower(power);
-                OuttakeMotorLeft.setPower(power);
+                double error = targetTicksPerSec - actualTicksPerSec;
+                double dt = timer.seconds();
+                timer.reset();
+
+                integralSum += error * dt;
+
+                double derivative = (error - lastError) / dt;
+                lastError = error;
+
+                double output = (12 * targetTicksPerSec)
+                        + (200 * error)
+                        + (8 * integralSum)
+                        + (1 * derivative);
+
+                output = output / 32767.0;
+                output = Math.max(-1, Math.min(1, output));
+
+                OuttakeMotorRight.setPower(output);
+                OuttakeMotorRight.setPower(-1*output);
                 telemetry.addLine("on");
             }
         }
