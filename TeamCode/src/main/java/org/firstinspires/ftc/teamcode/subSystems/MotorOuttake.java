5b58c0c10abf8556b025413862d0387caadbd8df
Kisutz <danm2nfamily@gmail.com>
2026-02-28 23:14:00 -0800

    pid tuned outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/MotorOuttake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/MotorOuttake.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.subSystems;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -8,15 +10,12 @@ import robotcore.Subsystem;
 
 
 public class MotorOuttake extends Subsystem {
-
     boolean isOn;
     public DcMotorEx OuttakeMotorLeft, OuttakeMotorRight;
-
-
     double lastError = 0;
     double integralSum = 0;
-    double rpm = 3000;
-
+    public static double rpm = 3000;
+    FtcDashboard dashboard;
     ElapsedTime timer = new ElapsedTime();
 
 
@@ -32,58 +31,67 @@ public class MotorOuttake extends Subsystem {
 
         OuttakeMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         OuttakeMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        dashboard = FtcDashboard.getInstance();
     }
 
     public void runOuttake() {
-        if(gamepad1.rightBumperWasPressed()){
-            rpm += 100;
-            if(rpm > 1.0){
-                rpm = 1.0;
-            }
-        }
-        else if(gamepad1.leftBumperWasPressed()){
-            rpm -= 100;
-            if(rpm < 0.0){
-                rpm = 0.0;
-            }
-        }
-
-
-
         double targetTicksPerSec = (rpm / 60.0) * 28.0;
         double actualTicksPerSec = -1*OuttakeMotorLeft.getVelocity();
         double actualRPM = (actualTicksPerSec / 28.0) * 60.0;
+
+        if(gamepad1.rightBumperWasPressed()){
+            rpm += 100;
+        }
+        else if(gamepad1.leftBumperWasPressed()){
+            rpm -= 100;
+        }
+
+
         if (gamepad2.xWasPressed()) {
+            integralSum = 0;
+            lastError = 0;
+            timer.reset();
             if (isOn) {
                 isOn = false;
-                OuttakeMotorRight.setPower(0.0);
-                OuttakeMotorLeft.setPower(0.0);
-                telemetry.addLine("off");
             } else {
                 isOn = true;
-                double error = targetTicksPerSec - actualTicksPerSec;
-                double dt = timer.seconds();
-                timer.reset();
-
-                integralSum += error * dt;
-
-                double derivative = (error - lastError) / dt;
-                lastError = error;
-
-                double output = (12 * targetTicksPerSec)
-                        + (200 * error)
-                        + (8 * integralSum)
-                        + (1 * derivative);
-
-                output = output / 32767.0;
-                output = Math.max(-1, Math.min(1, output));
-
-                OuttakeMotorRight.setPower(output);
-                OuttakeMotorRight.setPower(-1*output);
-                telemetry.addLine("on");
             }
         }
+        if (isOn) {
+            double error = targetTicksPerSec - actualTicksPerSec;
+            double dt = timer.seconds();
+            timer.reset();
+
+            integralSum += error * dt;
+
+            double derivative = (error - lastError) / dt;
+            lastError = error;
+
+            double output = (12 * targetTicksPerSec)
+                    + (200 * error)
+                    + (8 * integralSum)
+                    + (1 * derivative);
+
+            output = output / 32767.0;
+            output = Math.max(-1, Math.min(1, output));
+
+            OuttakeMotorLeft.setPower(output);
+            OuttakeMotorRight.setPower(-1*output);
+        } else {
+            OuttakeMotorLeft.setPower(0);
+            OuttakeMotorRight.setPower(0);
+        }
+
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.put("rpm:", rpm);
+        packet.put("actualRPM", actualRPM);
+        dashboard.sendTelemetryPacket(packet);
+        telemetry.addData("rpm", rpm);
+        telemetry.update();
     }
 }
+
+
 
 
