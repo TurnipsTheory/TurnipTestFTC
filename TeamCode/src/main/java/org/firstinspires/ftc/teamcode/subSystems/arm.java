e41efa89abc092a3a22b6d30a552d2e060be47c8
Kisutz <danm2nfamily@gmail.com>
2026-09-06 23:24:37 -0700

    working arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/arm.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subSystems;
 
+import java.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -9,6 +10,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.PwmControl;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -22,9 +24,13 @@ import robotcore.Subsystem;
 public class arm extends Subsystem {
     public double segment1 = 5.25; // INCHES. final: finalizes vaianblr; public: everyone can use; =: important
     public double segment2 = 6;// in
-    public double h;
+    private double h = 3, v = 0, a, c, z, w = 0, r = 90/320.0;
+    private double range = 160;
+    private double theta = 0;
+    public Servo yaw, pitch1r, pitch1l, pitch2, wrist, slidel,slider;
+    int i = 0;
 
-    public Servo yaw, pitch1r, pitch1l, pitch2, wrist;
+
     AnalogInput yEncoder, p1Encoder, p2Encoder, wEncoder;
 
 
@@ -39,79 +45,124 @@ public class arm extends Subsystem {
         pitch1r = hardwareMap.get(Servo.class, "p1r");
         pitch1l = hardwareMap.get(Servo.class, "p1l");
         pitch2 = hardwareMap.get(Servo.class, "p2");
-//        wrist = hardwareMap.get(Servo.class, "w");
-//        pitch2.setDirection(Servo.Direction.REVERSE);
-        pitch1l.setDirection(Servo.Direction.REVERSE);
+        slidel = hardwareMap.get(Servo.class, "sl");
+        slider = hardwareMap.get(Servo.class, "sr");
 
+        wrist = hardwareMap.get(Servo.class, "w");
+//        pitch2.setDirection(Servo.Direction.REVERSE);
+        slider.setDirection(Servo.Direction.REVERSE);
+        pitch1l.setDirection(Servo.Direction.REVERSE);
+//        yaw.setDirection(Servo.Direction.REVERSE);
+
+
+    }
+
+    public void pitch1Set(double theta) {
+        pitch1r.setPosition(theta);
+        pitch1l.setPosition(theta);
+    }
+    public void horizontalSet(double theta) {
+        slidel.setPosition(theta);
+        slider.setPosition(theta);
     }
     public void armInit() { //gets it into a starting position
-        pitch1r.setPosition(0.195);
-        pitch1l.setPosition(0.195);
-        pitch2.setPosition(0.375);
-        yaw.setPosition(0);
-//        wrist.setPosition(2);
+        pitch1Set(0.195);
+        pitch2.setPosition(0.38);
+        yaw.setPosition(90/320.0);
+        wrist.setPosition(0.5);
+        horizontalSet(0);
+        telemetry.addLine(String.format(Locale.US, "yaw: %.4f", yaw.getPosition()));
+        telemetry.update();
 
+    }
+
+    public void horizontal(boolean intake) {
+        if (intake) {
+            horizontalSet(0);
+        } else {
+            horizontalSet(0.3);
+        }
     }
     public void armFold() { //folds arm up
-        pitch2.setPosition(0);
-        pitch1r.setPosition(1);
-        pitch1l.setPosition(1);
-
-
-//        pitch1.setPosition(2);
-//        pitch2.setPosition(4);
-//        yaw.setPosition(3);
-//        wrist.setPosition(2);
+        wrist.setPosition(0.5);
+        pitch1Set(1);
+        if (i >=35) {
+            pitch2.setPosition(0);
+        }
+        if (i >=40) { //60
+            yaw.setPosition((180/320.0));
+            horizontal(false);
+        }
+        if (i >= 80) {
+        ((PwmControl) pitch1l).setPwmDisable();
+        ((PwmControl) pitch1r).setPwmDisable();
+        ((PwmControl) pitch2).setPwmDisable();
+        ((PwmControl) slidel).setPwmDisable();
+        ((PwmControl) slider).setPwmDisable();
+        } else {
+            i+=1;
+        }
+        telemetry.addLine(String.format(Locale.US, "pitch2: %.4f", pitch2.getPosition()));
+        telemetry.addData("i: ", i);
+        telemetry.update();
     }
     public void moveArm(double translation, double rotation, double wrotation) {
-        h = h + translation/10;
-        if (h <= 3) {
-            h = 3;
-        if (h >= 11.25) {
-            h = 11.25;
+        ((PwmControl) pitch1l).setPwmEnable();
+        ((PwmControl) pitch1r).setPwmEnable();
+        ((PwmControl) pitch2).setPwmEnable();
+        ((PwmControl) slidel).setPwmEnable();
+        ((PwmControl) slider).setPwmEnable();
+        i = 0;
+        r = rotation;
+        w = wrotation;
+//        yaw.setPosition();
+        yaw.setPosition(r);
+        wrist.setPosition(w);
+
+
+        if (translation == 3) {
+            h = 10.5;
         }
-        double v = 0;
-        double z = Math.sqrt(Math.pow(h, 2) + Math.pow(v, 2));
-        double c = Math.toDegrees(Math.acos((Math.pow(h,2) - Math.pow(segment1, 2) - Math.pow(segment2,2))/(-2*segment1*segment2))); //finds the angle c by reversing law of cosines
-        double a = Math.abs(Math.toDegrees(Math.asin(Math.sin(c)*segment1/h) + Math.asin(v/z))/200); //finds the
+        if (translation == 2) {
+            h = 9;
+        }
+        if (translation == 1) {
+            h = 7.5;
+        }
+        c = Math.acos((Math.pow(h,2) - Math.pow(segment1, 2) - Math.pow(segment2,2))/(-2*segment1*segment2)); //finds the angle c by reversing law of cosines
+        a = Math.toDegrees(Math.asin((Math.sin(c)*segment2)/h)); //finds the
+
         if (Double.isNaN((a))) {
-            a = 89.3580679749/200;
+            a = 89.3580679749;
         }
-        double r = rotation;
-        double w = wrotation;
-//        pitch1.setPosition(a);
-        telemetry.addLine(String.format(Locale.US, "a: %.4f", a));
-//        telemetry.addLine(String.format(Locale.US, "a/150: %6.0f", a/150));
 
 
-        pitch1r.setPosition(0.195+a);
-        pitch1l.setPosition(0.195+a);
-//        pitch2.setPosition(c);
-//            yaw.setPosition(r);
-//        wrist.setPosition(w);
-//        if (gamepad2.aWasPressed()) {
+        c = Math.toDegrees(c);
+        pitch2.setPosition(0.38-((180-c)/320.0));
+        pitch1Set(0.195+(a/range));
+
+        telemetry.addLine(String.format(Locale.US, "translation: %.4f", translation));
+        telemetry.addLine(String.format(Locale.US, "rotation: %.4f", rotation));
+        telemetry.addLine(String.format(Locale.US, "wrist: %.4f", wrotation));
+        telemetry.addLine(String.format(Locale.US, "yaw: %.4f", yaw.getPosition()));
+        telemetry.update();
 //
-//        } else{
-////
-//        }
-    }
-//    public void armRead() {
-//        double voltage = yEncoder.getVoltage(); // Returns voltage (typically 0.0 to 3.3V)
-//        double maxVoltage = yEncoder.getMaxVoltage();
-//        double positionFraction = voltage / maxVoltage;
-//        telemetry.addLine(String.format(Locale.US, "y: %6.0f", positionFraction));
-//        voltage = p1Encoder.getVoltage(); // Returns voltage (typically 0.0 to 3.3V)
-//        maxVoltage = p1Encoder.getMaxVoltage();
-//        positionFraction = voltage / maxVoltage;
-//        telemetry.addLine(String.format(Locale.US, "p1: %6.0f", positionFraction));
-//        voltage = p2Encoder.getVoltage(); // Returns voltage (typically 0.0 to 3.3V)
-//        maxVoltage = p2Encoder.getMaxVoltage();
-//        positionFraction = voltage / maxVoltage;
-//        telemetry.addLine(String.format(Locale.US, "p2: %6.0f", positionFraction));
-//        voltage = wEncoder.getVoltage(); // Returns voltage (typically 0.0 to 3.3V)
-//        maxVoltage = wEncoder.getMaxVoltage();
-//        positionFraction = voltage / maxVoltage;
-//        telemetry.addLine(String.format(Locale.US, "w: %6.0f", positionFraction));
-//
-    }
+        }
+        public void collect() {
+            v = -4.25;
+            z = Math.sqrt(Math.pow(h, 2) + Math.pow(v, 2));
+            c = Math.acos((Math.pow(h,2) - Math.pow(segment1, 2) - Math.pow(segment2,2))/(-2*segment1*segment2)); //finds the angle c by reversing law of cosines
+            a = (Math.toDegrees(Math.asin(Math.sin(c)*segment2/z)) + Math.toDegrees(Math.asin(v/z)))/range;
+            c = Math.toDegrees(c);
+            telemetry.addLine(String.format(Locale.US, "a: %.4f", a));
+            telemetry.addLine(String.format(Locale.US, "h: %.4f", h));
+            telemetry.addLine(String.format(Locale.US, "v: %.4f", v));
+            telemetry.addLine(String.format(Locale.US, "c: %.4f", c));
+
+            telemetry.update();
+
+            pitch1Set(0.195+a);
+            pitch2.setPosition(0.38-((180-c)/320.0));
+        }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/fieldCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/fieldCentricDrive.java
@@ -5,34 +5,76 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.subSystems.fieldCentricMecanum;
 import org.firstinspires.ftc.teamcode.subSystems.arm;
+
+import java.util.Locale;
+
 @TeleOp(name="field centric drive")
 public class fieldCentricDrive extends OpMode{
-    double oldTime = 0;
+    public double oldTime = 0;
     public final double num = 10;
+    public boolean collecting = false, intake = true;
 
     fieldCentricMecanum drive = new fieldCentricMecanum();
     arm arm = new arm();
-    double axial, lateral, yaw, translation, rotation, wrist;
+    double axial, lateral, yaw, translation, rotation = 90/320.0, wrist = 0.5;
 
     public void init() {
+        translation = 1;
         drive.init(hardwareMap, telemetry);
         arm.init(this);
+        arm.horizontal(intake);
         arm.armInit();
     }
 
     public void loop() {
-
         axial = gamepad1.left_stick_y;
         lateral = -gamepad1.left_stick_x;
         yaw = gamepad1.right_stick_x;
 
-        translation = gamepad2.left_stick_y;
-        rotation = gamepad2. left_stick_x;
-        wrist = gamepad2.right_stick_x;
+
+        if (intake) {
+            arm.horizontal(true);
+            if (!collecting) {
+                if (gamepad2.right_bumper && wrist >= 0.31 && wrist < 0.68) {
+                    wrist += 0.01;
+                } else if (gamepad2.left_bumper && wrist > 0.32 && wrist <= 0.69) {
+                    wrist -= 0.01;
+                }
+                if (gamepad2.left_stick_x > 0 && rotation <= 180/320.0 || gamepad2.left_stick_x < 0 && rotation >= 0) {
+                    rotation = rotation + gamepad2.left_stick_x/100.0;
+                }
+//                if (gamepad2.dpad_right && rotation > 0 && rotation <= 180/320.0 + 0.01) {
+//                    rotation -= 0.01;
+//                } else if (gamepad2.dpad_left && rotation >= -0.01 && rotation < 180/320.0) {
+//                    rotation += 0.01;
+//                }
+                if (gamepad2.dpadUpWasPressed() && translation >= 1 && translation != 3) {
+                    translation += 1;
+                } else if (gamepad2.dpadDownWasPressed() && translation <= 3 && translation != 1) {
+                    translation -= 1;
+                }
+                arm.moveArm(translation, rotation, wrist);
+            } else {
+                arm.collect();
+            }
+
+            if (gamepad2.aWasPressed()) {
+                collecting = !collecting;
+            }
+        } else {
+            rotation = 90/320.0;
+            wrist = 0.5;
+            arm.armFold();
+        }
+
+        if (gamepad2.bWasPressed()) {
+            intake = !intake;
+            if (!intake) {
+                collecting = false;
+            }
+        }
 
         drive.fieldCentric(axial, lateral, yaw);
-        arm.moveArm(translation, rotation, wrist);
-//        arm.armFold();
 
         double newTime = getRuntime();
         double loopTime = newTime-oldTime;
